63997e7fc9a2d797b36131d0eb9f867dfdffd7c4
helenavuv <102721931+helenavuv@users.noreply.github.com>
2022-05-07 02:07:46 +0200

    Add files via upload

--- js/atributi.js
+++ js/atributi.js
@@ -9,3 +9,21 @@ function Spremi()
 	var json = JSON.stringify(data);
 	console.log(json);
 }
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+function Spremi()
+{
+    var data = $("input").val()
+    var data = {podatak:data};
+
+   var jsonData = JSON.stringify(data); //tip+":"+podatak
+
+    download(jsonData, 'podatak.json', 'text/json');
+
+}
