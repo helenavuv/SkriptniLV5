9a45bb57afdc582f9b8b77aad483c5d01b478e64
helenavuv <102721931+helenavuv@users.noreply.github.com>
2022-05-07 02:08:41 +0200

    Add files via upload

--- js/atributi.js
+++ js/atributi.js
@@ -2,13 +2,6 @@ $("select").change(function(){
 	$("input").attr('type',$("select").val());
 });
 
-function Spremi()
-{
-	var value=$("input").val()
-	var data={podatak:value};
-	var json = JSON.stringify(data);
-	console.log(json);
-}
 function download(content, fileName, contentType) {
     var a = document.createElement("a");
     var file = new Blob([content], {type: contentType});
